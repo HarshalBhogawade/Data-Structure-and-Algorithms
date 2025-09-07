1f3927ca41671e327c63097ab6a43c4517fd3154
HarshalBhogawade <harshalbhogawade1@gmail.com>
2025-09-07 23:37:30 +0530

    CF

---
+++ Codeforces/Div3-1047/Collatz Conjecture.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n ;
+    cin>>n;
+    while(n--){
+        int k,x;
+        cin>>k>>x;
+        while(k--){
+            x*=2;
+        }
+        cout<<x<<endl; 
+    }
+    return 0;
+}
